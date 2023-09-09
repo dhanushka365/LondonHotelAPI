86f7225f367522f5f394ec720a79928064eab36b
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-09 13:14:36 +0530

    Add project files.

---
+++ LondonHotelAPI/Controllers/RootController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LondonHotelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = "www.google.lk"
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
